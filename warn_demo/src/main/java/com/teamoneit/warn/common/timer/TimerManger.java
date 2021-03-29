731268a1e5707a023f8f2e07a3131bec4408b67c
593058261@qq.com <593058261@qq.com>
2021-03-29 21:43:40 +0800

    自己修改

--- warn_demo/src/main/java/com/teamoneit/warn/common/timer/TimerManger.java
+++ warn_demo/src/main/java/com/teamoneit/warn/common/timer/TimerManger.java
@@ -21,8 +21,8 @@ public class TimerManger {
 
     //第一次启动延迟1分钟启动，启动之后方法结束延迟15分钟启动 1000毫秒就是1秒
     // @Scheduled(cron = "0 0 * * * ? *")
-    @Scheduled(initialDelay = 1000 * 60 * 1, fixedDelay = 1000 * 60 * 15)
-    @Async
+//    @Scheduled(initialDelay = 1000 * 60 * 1, fixedDelay = 1000 * 60 * 15)
+//    @Async
     public void pushExceptionTask() {
         try {
             log.info("推送任务开始");
@@ -36,8 +36,8 @@ public class TimerManger {
     }
 
     //     @Scheduled(cron = "0 40 * * * ? *")
-    @Scheduled(initialDelay = 1000 * 60 * 1, fixedDelay = 1000 * 60 * 15)
-    @Async
+//    @Scheduled(initialDelay = 1000 * 60 * 1, fixedDelay = 1000 * 60 * 15)
+//    @Async
     public void changeExceptionTaskStatus() {
         try {
             log.info("修改状态开始");
